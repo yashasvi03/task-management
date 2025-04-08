a4201bb5ac0209799a780f29da319d4277445ea9
Yashasvi Suryavansh <yashasvi03suryavansh@gmail.com>
2025-04-08 20:31:12 +0530

    Frontend linking to backend

--- frontend/src/services/taskService.ts
+++ frontend/src/services/taskService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Task, CreateTaskDto, UpdateTaskDto } from '../types/task';
 
-const API_URL = 'http://localhost:5000/api/tasks';
+const API_URL = '${process.env.REACT_APP_API_URL}/api/tasks';
 
 export const getAllTasks = async (): Promise<Task[]> => {
   const response = await axios.get<Task[]>(API_URL);
